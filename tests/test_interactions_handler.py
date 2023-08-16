727021e474d0244a4643fcc6dc8a3dcb777e3c66
chawkk6404 <chawk_k@outlook.com>
2023-08-16 11:57:39 -0700

    Add new tests

--- tests/test_interactions_handler.py
+++ tests/test_interactions_handler.py
@@ -1,50 +1,108 @@
-from typing import Any, Mapping
+import asyncio
+import json
+from typing import Any, MutableMapping
 
 import discord
 import pytest
+from discord import app_commands
 
 import aiointeractions
 
-
-class MockRequest:
-    def __init__(self, headers: Mapping[str, Any], data: Any):
-        self.headers = headers
-        self._data = data
-
-    async def text(self) -> str:
-        return str(self._data)
+with open('tests/mock_interaction.json', 'r') as f:
+    data = json.load(f)
 
 
 class MockApp(aiointeractions.InteractionsApp):
-    verification: str = '{}{"type": 1}'
+    verification: tuple[str, ...] = (json.dumps({'type': 1}), json.dumps(data))
 
-    def _verify_request(self, headers: Mapping[str, Any], body: str) -> bool:
-        return f'{headers}{body}' == self.verification
-
-
-intents = discord.Intents.none()
-client = discord.Client(intents=intents)
-tree = discord.app_commands.CommandTree(client)
-
-app = MockApp(client)
+    def _verify_request(self, headers: MutableMapping[str, Any], body: str) -> bool:
+        return str(body) in self.verification
 
 
 @pytest.mark.asyncio
-async def test_no_verification() -> None:
-    request = MockRequest({}, '')
-    response = await app.interactions_handler(request)  # type: ignore
+async def test_no_verification(aiohttp_client) -> None:
+    intents = discord.Intents.none()
+    discord_client = discord.Client(intents=intents)
+    app = MockApp(discord_client)
+
+    client = await aiohttp_client(app.app)
+    response = await client.post('/interactions', headers={}, data='')
+
     assert response.status == 401
 
 
 @pytest.mark.asyncio
-async def test_invalid_verification() -> None:
-    request = MockRequest({}, 'badtext')
-    response = await app.interactions_handler(request)  # type: ignore
+async def test_invalid_verification(aiohttp_client) -> None:
+    intents = discord.Intents.none()
+    discord_client = discord.Client(intents=intents)
+    app = MockApp(discord_client)
+
+    client = await aiohttp_client(app.app)
+    response = await client.post('/interactions', headers={}, data='badtext')
+
     assert response.status == 401
 
 
 @pytest.mark.asyncio
-async def test_valid_verification() -> None:
-    request = MockRequest({}, '{"type": 1}')
-    response = await app.interactions_handler(request)  # type: ignore
-    assert response.status == 200
+async def test_invalid_verification_exception(aiohttp_client) -> None:
+    intents = discord.Intents.none()
+    discord_client = discord.Client(intents=intents)
+    app = MockApp(discord_client, raise_for_bad_response=True)
+
+    client = await aiohttp_client(app.app)
+    response = await client.post('/interactions', headers={}, data='badtext')
+
+    assert response.status == 401
+
+
+@pytest.mark.asyncio
+async def test_valid_verification_ping(aiohttp_client) -> None:
+    intents = discord.Intents.none()
+    discord_client = discord.Client(intents=intents)
+    app = MockApp(discord_client)
+
+    client = await aiohttp_client(app.app)
+    response = await client.post('/interactions', headers={}, json={'type': 1})
+
+    assert response.status == 200 and json.loads(await response.text())['type'] == 1
+
+
+@pytest.mark.asyncio
+async def test_command(aiohttp_client) -> None:
+    intents = discord.Intents.none()
+    discord_client = discord.Client(intents=intents)
+    discord_client.loop = asyncio.get_running_loop()
+
+    app = MockApp(discord_client)
+    tree = app_commands.CommandTree(discord_client)
+
+    changed = False
+
+    @tree.command()
+    async def test_command(interaction: discord.Interaction) -> None:
+        nonlocal changed
+        changed = True
+
+    client = await aiohttp_client(app.app)
+    await client.post('/interactions', headers={}, json=data)
+
+    assert changed is True
+
+
+@pytest.mark.asyncio
+async def test_success_response(aiohttp_client) -> None:
+    intents = discord.Intents.none()
+    discord_client = discord.Client(intents=intents)
+    discord_client.loop = asyncio.get_running_loop()
+
+    app = MockApp(discord_client, success_response=lambda r: 'The Master is cool')
+    tree = app_commands.CommandTree(discord_client)
+
+    @tree.command()
+    async def test_command(interaction: discord.Interaction) -> None:
+        print('The Master is cool')
+
+    client = await aiohttp_client(app.app)
+    response = await client.post('/interactions', headers={}, json=data)
+
+    assert response.status == 200 and await response.text() == 'The Master is cool'
---
+++ tests/test_running.py
@@ -0,0 +1,15 @@
+import discord
+import pytest
+
+import aiointeractions
+
+intents = discord.Intents.none()
+client = discord.Client(intents=intents)
+app = aiointeractions.InteractionsApp(client)
+
+
+@pytest.mark.asyncio
+async def test_set_running() -> None:
+    assert app.is_running() is False
+    await app._set_running(None)
+    assert app.is_running() is True
